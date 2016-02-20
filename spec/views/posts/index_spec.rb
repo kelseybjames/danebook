d55979b0215a698f4f74289b4106aae803dd897e
Kelsey James <kelseybjames@gmail.com>
2016-02-19 16:44:13 -0800

    fixed posts index test

--- spec/views/posts/index_spec.rb
+++ spec/views/posts/index_spec.rb
@@ -22,9 +22,42 @@ describe 'posts/index.html.erb' do
 
     it 'logged in user can see posts' do
       assign(:user, user)
+      assign(:post, create(:post, user: user))
       assign(:posts, posts)
       render
-      expect(rendered).to match("#{secret.author.name}")
+      expect(rendered).to match("#{posts.first.body}")
+    end
+
+    it 'current user can post on timeline' do
+      assign(:user, user)
+      assign(:post, Post.new(user: user))
+      assign(:posts, posts)
+      render
+      expect(rendered).to match("Create Post")
+    end
+
+    it 'user can like timeline post' do
+      assign(:user, user)
+      assign(:post, Post.new(user: user))
+      assign(:posts, posts)
+      render
+      expect(rendered).to match("Like")
+    end
+
+    it 'user can comment on timeline post' do
+      assign(:user, user)
+      assign(:post, Post.new(user: user))
+      assign(:posts, posts)
+      render
+      expect(rendered).to match("Create Comment")
+    end
+
+    it 'current user can delete timeline post' do
+      assign(:user, user)
+      assign(:post, Post.new(user: user))
+      assign(:posts, posts)
+      render
+      expect(rendered).to match("Delete")
     end
   end
 end
